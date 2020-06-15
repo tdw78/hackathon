0139f5a30fad7a0140bf391f9e4418d68c9c60e1
Tim Weiss <tdweiss1978@gmail.com>
2020-06-14 20:59:04 -0700

    Setup Complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Landing from './components/Landing';
-import Library from './components/Library';
-import Album from './components/Album';
 import { Navbar } from 'reactstrap';
-
-
 
 
 class App extends Component {
